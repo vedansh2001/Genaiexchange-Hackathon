a43737df13a1803e928a7fe014b0c08a7eb6058d
Vedansh Mishra <vedanshm2001@gmail.com>
2024-09-26 22:13:02 +0530

    add initial files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{ backgroundColor: '#1E201E', height:'1000px'}}>
+        <div style={{backgroundColor: '#40534C', height:'80px'}} >
+          User
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
