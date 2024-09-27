7042d3b3a20f24ee5bf76f42cad606f44e39d607
Vedansh Mishra <vedanshm2001@gmail.com>
2024-09-27 12:16:39 +0530

    updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <div style={{ backgroundColor: '#1E201E', height:'1000px'}}>
-        <div style={{backgroundColor: '#40534C', height:'80px'}} >
-          User
+      <div className='bg-[#212321] h-screen'>
+        <div className='bg-[#40534C] h-[80px] flex justify-end pr-[24px] pt-[18px]'>
+          <a href="https://extractor-mfys.onrender.com/" target="_blank" rel="noopener noreferrer">
+            <img src="https://freesvg.org/img/abstract-user-flat-4.png" alt="Clickable Image" className='w-[50px] h-[50px]' />
+          </a>
         </div>
+
+        {/* Fixed image at bottom left */}
+        <a href="#" target="_blank" className="fixed bottom-6 right-6 z-50" rel="noopener noreferrer">
+          <img src="https://imgs.search.brave.com/4mXW4wvFtVzf4gIyOTbGb5BejaFHPR9-xOAisPrCqfA/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzg1Lzc2LzAz/LzM2MF9GXzI4NTc2/MDM3NV84V1p6NFgy/SnY1NEdtSDdtZkJm/dENkbEhTUDg2SlRp/SC5qcGc" 
+          alt="Clickable Image" className='w-[50px] h-[50px] rounded-3xl' />
+        </a>
       </div>
     </>
   )
